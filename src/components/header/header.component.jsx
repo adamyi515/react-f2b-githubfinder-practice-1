d1bf97b58d3cbbb27f959aa8dad3bf290b383e9d
0mni-gears515 <adamyi515@gmail.com>
2022-11-08 10:25:15 -0600

    Finish header component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import './header.style.css';
 import { FaGithub } from 'react-icons/fa';
 
+// react-router-dom
+import { Link } from 'react-router-dom';
+
 const Header = () => {
     return(
         <header className='header'>
             <div>
-                <FaGithub />
+                <FaGithub /> Github Finder
             </div>
-            <ul className='header-menus'>
-                <li>HOME</li>
-                <li>ABOUT</li>
+            <ul className='header-menu'>
+                <li>
+                    <Link to='/' className='menu-item__link'>HOME</Link>
+                </li>
+                <li>
+                    <Link to='/about'  className='menu-item__link'>ABOUT</Link>
+                </li>
             </ul>
         </header>
     )
